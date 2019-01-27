6c9f833bebd1261fe110a2ec5692b7db1e245b9e
Page Harrison <page.harrison.o@gmail.com>
2019-01-26 19:01:05 -0500

    Fix url links in main.

--- teemup.com/main.py
+++ teemup.com/main.py
@@ -4,23 +4,23 @@ from db import session, User
 app = Flask(__name__, static_folder="static")
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home.html")
 def home():
     return render_template("home.html")
 
-@app.route("/account")
+@app.route("/account.html")
 def account():
     return render_template("account.html")
 
-@app.route("/create")
+@app.route("/create.html")
 def create():
     return render_template("create.html")
 
-@app.route("/find")
+@app.route("/find.html")
 def find():
     return render_template("find.html")
 
-@app.route("/yourEvents")
+@app.route("/yourEvents.html")
 def yourEvents():
     return render_template("yourEvents.html")
 
